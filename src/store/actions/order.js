fb8c62246d5490a6692cf47860b3748dc7b49f31
Nick Jacob <njacob341@gmail.com>
2021-01-23 11:25:49 -0800

    Working on order actions

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -13,11 +13,18 @@ export const purchaseBurgerFail = (error) => {
     return {
         type: actionTypes.PURCHASE_BURGER_FAIL,
         error: error,
+    }; 
+};
+
+export const purchaseBurgerStart = () => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_START
     };
 };
 
-export const purchaseBurgerStart = (orderData) => {
+export const purchaseBurger = (orderData) => {
     return dispatch => {
+       dispatch(purchaseBurgerStart());
         axios.post('/orders.json', orderData)
             .then(response => {
                 console.log(response.data);
